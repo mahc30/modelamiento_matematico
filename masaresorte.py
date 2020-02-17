2ab93d813baf15ba5ee9218dacd3d084fc688989
mahc30 <48729946+mahc30@users.noreply.github.com>
2020-02-16 20:57:26 -0500

    ver3

--- masaresorte.py
+++ masaresorte.py
@@ -6,27 +6,40 @@ import math
 def masaresorte(X, t):
     #Masas
     m1 = 20
-    m2 = 10
-    beta = 0.4 #Coeficiente de Friccioń
+    m2 = 20
+    beta = 1 #Coeficiente de Friccioń
     #Constantes de Elasticidad
-    k1 = 100
-    k2 = 100
+    k1 = 20
+    k2 = 20
     #Movimiento cosenoidal (Multiplico por 3 para hacerlo más 'amplio')
-    cos = 3*math.cos(t)
+    scale = 3
+    cos = scale * math.cos(t)
 
-    x1,dx1, x2, dx2 = X
+    #Después de unas oscilaciones, Se deja de mover el resorte 1
+    
+    if(t >= 120):
+        cos = 0
 
-    dv1dt = (-k1 * cos - k2*x1 + k2*x2 + beta*dx1)/m1
-    dv2dt = (-k2*x2 + k2*x1 + beta*dx2)/m2
+    x1,dx1, dx2 = X
 
-    return [dx1, dv1dt, dx2, dv2dt]
+    #Ecuaciones
+    #Ecuación para la masa 1
+    dv1dt = (-k1 * (x1 + cos) + k2*(dx2 - dx1) + beta*dx1)/m1
+    #Ecuación para la masa 2
+    dv2dt = (k2*(dx1 - dx2) + beta*dx2)/m2
 
-t = np.linspace(0,30,1000)
-y = odeint(masaresorte, [3,0,5,0], t)
+    return [dx1, dv1dt,dv2dt]
 
-#y[1,3] -> Gráfica de la aceletación en función del tiempo
-plt.plot(t,y[:,1])
-plt.plot(t,y[:,3])
+#Simulo desde 90 para que el valor inicial de coseno sea 0
+t = np.linspace(90,180,10000)
+
+#Asumiendo que los resortes tienen su elongación natural acorde a la distancia entre un carrito y el otro
+#El primer argumento es una elongación inicial del primer resorte, el segundo una velocidad incial para la primera masa, el tercero una velocidad inicial para la segunda masa
+y = odeint(masaresorte, [10, 0, 0], t)
+
+#y[1,3] -> Gráfica de la aceleración en función del tiempo
+plt.plot(t,y[:,0])
+plt.plot(t,y[:,2])
 #y[0,2] -> Grafica de la velocidad en función del tiempo
 '''
 plt.plot(t,y[:,0])
